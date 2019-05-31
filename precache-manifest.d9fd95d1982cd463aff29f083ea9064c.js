b9d08b55a34e1946364624eefe5dd1b078894e90
ricardo <ricardo.9882@gmail.com>
2019-05-30 22:13:31 -0500

    Updates

---
+++ precache-manifest.d9fd95d1982cd463aff29f083ea9064c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9543ec6d3bdf6a80d4bfbd6e5ad923c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1f5086fed5ec9142931",
+    "url": "/static/css/main.eb24b12e.chunk.css"
+  },
+  {
+    "revision": "01c5fd3717831a7646e1",
+    "url": "/static/js/2.c719dd68.chunk.js"
+  },
+  {
+    "revision": "d1f5086fed5ec9142931",
+    "url": "/static/js/main.97ef50b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7845dcbd3d7918196b3e82583fa49b14",
+    "url": "/static/media/banner-kasa.7845dcbd.jpg"
+  },
+  {
+    "revision": "8cade6233fe27e3dd272484afa4402e7",
+    "url": "/static/media/header-auto.8cade623.png"
+  },
+  {
+    "revision": "2c9d4ed298632db58ff073858f2be102",
+    "url": "/static/media/honda-logo.2c9d4ed2.png"
+  },
+  {
+    "revision": "5abd7de7df0f701f3bb582e33afae0a5",
+    "url": "/static/media/icon-phone.5abd7de7.svg"
+  },
+  {
+    "revision": "1c10706197b2d01d6fa25016caebaa8f",
+    "url": "/static/media/icon-pointer.1c107061.svg"
+  },
+  {
+    "revision": "661aeaa4ade56223a8553de4e9249652",
+    "url": "/static/media/icon-whatsapp.661aeaa4.svg"
+  },
+  {
+    "revision": "26743a77bbfe1e6d79dc1815dc2c4d72",
+    "url": "/static/media/img-mapa.26743a77.jpg"
+  },
+  {
+    "revision": "a886fd618b59f74cfb1e0020878db838",
+    "url": "/static/media/kasa-logo-hr.a886fd61.svg"
+  },
+  {
+    "revision": "64876d6eb9ff94b84cb9991dbe9230ca",
+    "url": "/static/media/kasa-logo.64876d6e.svg"
+  }
+]);
